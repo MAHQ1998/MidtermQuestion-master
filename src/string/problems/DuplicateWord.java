1e7b52af20b57cf3984f53855a4a7427f0f8e4d2
Shadman Hoque <shadman@Shadmans-iMac.local>
2020-11-08 16:08:06 -0500

    Add duplicat word

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -9,6 +12,43 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        int count = 0;
+        double sum = 0;
+        double average = 0;
+
+        String[] duplicateData = st.split(" ");
+
+        HashMap<String, Integer> occurrences = new HashMap<>();
+
+        for (int i = 0; i < duplicateData.length; i++) {
+            Integer previousCount = occurrences.get(duplicateData[i]);
+            if (previousCount == null) {
+                previousCount = 0;
+            }
+
+            occurrences.put(duplicateData[i], previousCount + 1);
+        }
+
+        System.out.println(occurrences);
+
+        System.out.println(occurrences.get("Java"));
+
+
+        Scanner scanner = new Scanner(st);
+
+        while (scanner.hasNext()) {
+
+            String userInput = scanner.next();
+
+            double charNum = userInput.length();
+            sum = charNum + sum;
+            count++;
+
+            if (count > 0) {
+                average = sum / count;
+            }
+        }
+        System.out.println("Average word length = " + average);
 
     }
 
