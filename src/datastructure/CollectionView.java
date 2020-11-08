d46d3d2c527671c9d9b6d6dab692d595f9a337d9
Shadman Hoque <shadman@Shadmans-iMac.local>
2020-11-08 13:21:05 -0500

    1st commit

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -1,5 +1,6 @@
 package datastructure;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CollectionView {
@@ -7,6 +8,9 @@ public class CollectionView {
     public static void main(String[] args) {
         /*
          Map is created and inserted some data.Retrieve the Collection view of values present in map
+
+        Ans: Map we can call the value based on Key/Values
+        in order to Retrieve ..
          */
         HashMap<Integer, String> map = new HashMap<Integer, String>();
         map.put(1, "NYC");
@@ -15,6 +19,13 @@ public class CollectionView {
         map.put(4, "Boston");
         map.put(5, "San Jose");
         map.put(6, "Seattle");
+
+        ArrayList<HashMap<Integer, String>> newMap = new ArrayList<>();
+        newMap.add(map);
+
+        System.out.println(map);
+
+        //System.out.println(map.get(1));
 
     }
 
